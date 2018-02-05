b66e420bb294e8970a74a6a1fbbe088a542308f7
rafalciesla <ra.ciesla@wp.pl>
2018-02-05 17:57:09 +0100

    .

--- src/main/java/com/ciesla/marketcheckoutmicroservice/controller/ProductController.java
+++ src/main/java/com/ciesla/marketcheckoutmicroservice/controller/ProductController.java
@@ -6,10 +6,7 @@ import com.ciesla.marketcheckoutmicroservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -29,7 +26,7 @@ public class ProductController {
         return productService.findAllProducts();
     }
 
-    @GetMapping("add/" + "{name}" + "/" + "{price}" + "/" + "{type}" + "/" + "{quantity}")
+    @PostMapping("add/" + "{name}" + "/" + "{price}" + "/" + "{type}" + "/" + "{quantity}")
     public ResponseEntity<String> addNewProductToDatabase(@PathVariable("name") String name,
                                                   @PathVariable("price") Double price,
                                                   @PathVariable("type") Integer type,
